86e1b4f4b56025b108ba9e7239e3365817e1e06f
ryand88 <ryand8@hotmail.com>
2017-06-28 16:28:04 -0600

    finished siri project

---
+++ server.js
@@ -0,0 +1,45 @@
+/*
+npm start server.js  (start the server)
+
+curl -i http://localhost:8887  (get a response from server)
+*/
+var express = require('express');
+var app = express();
+
+app.listen(8887, function() {
+    console.log('Listening on port 8887');
+});
+
+var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that."];
+
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
+    message: getRandomMessage()
+  }));
+});
+
+function getRandomMessage(){
+    var i = Math.floor(Math.random() * 3);
+    return messages[i];
+}
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
